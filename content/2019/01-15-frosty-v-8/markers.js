7b7c61fac8cd34d7d4f54913b769019e8118940d
Eric Scouten <eric@scouten.com>
2021-11-13 19:32:47 -0800

    Frosty V-8.

---
+++ content/2019/01-15-frosty-v-8/markers.js
@@ -0,0 +1,5 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-3851-010", 47.606888, -122.547233),
+    ]
+}
---
+++ content/2019/_index.md
@@ -0,0 +1,5 @@
++++
+title = "2019"
+sort_by = "date"
+transparent = true
++++
