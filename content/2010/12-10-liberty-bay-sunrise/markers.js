768efc12b8299d0b00c991c0ae7d631b7f8edcd9
Eric Scouten <eric@scouten.com>
2022-04-03 13:53:59 -0700

    Liberty Bay Sunrise.

---
+++ content/2010/12-10-liberty-bay-sunrise/markers.js
@@ -0,0 +1,6 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-1762-001", 47.727213, -122.640968),
+        addGpxMarker(map, "es-1762-002", 47.727213, -122.640968),
+    ]
+}
