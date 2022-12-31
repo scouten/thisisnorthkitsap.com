99a9c1d2d3d06b6ed3f64b5d59012a67c290d51c
Eric Scouten <git@scouten.me>
2022-12-31 14:08:43 -0800

    Liberty Bay at Dusk

---
+++ content/2022/12-08-liberty-bay-at-dusk/markers.js
@@ -0,0 +1,6 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4456-006", 47.735753, -122.649247),
+        addGpxMarker(map, "es-4457-002", 47.733353, -122.646486),
+    ]
+}
