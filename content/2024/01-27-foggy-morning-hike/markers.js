f82b68b9ceab1a5721d71c3da0c78c234938fbd4
Eric Scouten <git@scouten.me>
2024-03-24 11:52:10 -0700

    Foggy Morning Hike

---
+++ content/2024/01-27-foggy-morning-hike/markers.js
@@ -0,0 +1,6 @@
+function addGpxMarkers(map) {
+    return [
+        addGpxMarker(map, "es-4630-004", 47.781333, -122.538833),
+        addGpxMarker(map, "es-4630-006", 47.781333, -122.538667),
+    ]
+}
